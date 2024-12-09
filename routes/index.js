1b1ac13cc1b3c69fe4d93c54c91d01e3ee66107a
Nathaniel Daley <ndaley64@gmail.com>
2024-12-09 16:28:11 -0600

    add signin and signup routes and remove user routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRounter = require("./clothingItems");
 const { INVALID_ROUTE_ERROR, NOT_FOUND_STATUS } = require("../utils/errors");
+const { login, createUser } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRounter);
+router.use("/signin", login);
+router.use("/signup", createUser);
 
 router.use((request, response, next) => {
   response.status(NOT_FOUND_STATUS).send({ message: INVALID_ROUTE_ERROR });
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,4 @@
 const router = require("express").Router();
 const { getUsers, getUser, createUser } = require("../controllers/users");
 
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-router.post("/", createUser);
-
 module.exports = router;
