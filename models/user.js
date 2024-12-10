bf410e4939f23402395269c251f098b818031924
Nathaniel Daley <ndaley64@gmail.com>
2024-12-09 20:32:26 -0600

    ensure user password is not returned

--- models/user.js
+++ models/user.js
@@ -35,6 +35,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "The user's password is required"],
+    select: false,
   },
 });
 
@@ -42,18 +43,23 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error(LOGIN_ERROR));
-    }
-
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error(LOGIN_ERROR));
       }
-      return user;
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error(LOGIN_ERROR));
+        }
+
+        delete user.password;
+
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("user", userSchema);
