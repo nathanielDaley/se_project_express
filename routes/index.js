7be39eccfad76424731f4238811b7d41ff509ab1
Nathaniel Daley <ndaley64@gmail.com>
2024-12-09 20:12:32 -0600

    use users router for me routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
-const clothingItemRounter = require("./clothingItems");
+
+const userRouter = require("./users");
+const clothingItemRouter = require("./clothingItems");
+
+const { login, createUser } = require("../controllers/users");
+
 const { INVALID_ROUTE_ERROR, NOT_FOUND_STATUS } = require("../utils/errors");
-const { login, createUser, getUser } = require("../controllers/users");
 
-const auth = require("../middlewares/auth");
-
-router.use("/users/me", auth, getUser);
-router.use("/items", clothingItemRounter);
-router.use("/signin", login);
-router.use("/signup", createUser);
+router.use("/users", userRouter);
+router.use("/items", clothingItemRouter);
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use((request, response, next) => {
   response.status(NOT_FOUND_STATUS).send({ message: INVALID_ROUTE_ERROR });
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 const { getUsers, getUser, createUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
+
+router.get("/me", auth, getUser);
 
 module.exports = router;
