d0f0096828647049c98dab8e928b3c6f9bf1d4d4
Nathaniel Daley <ndaley64@gmail.com>
2025-01-08 16:50:08 -0600

    send error to centralize route handler when request sent to invalid route

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,12 @@ const clothingItemRouter = require("./clothing-items");
 
 const { login, createUser } = require("../controllers/users");
 
-const { INVALID_ROUTE_ERROR, NOT_FOUND_STATUS } = require("../utils/errors");
+const { INVALID_ROUTE_ERROR } = require("../utils/errors");
 const {
   validateUserBody,
   validateUserCredentials,
 } = require("../middlewares/validation");
+const NotFoundError = require("../errors/not-found-error");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
@@ -17,8 +18,7 @@ router.post("/signin", validateUserCredentials, login);
 router.post("/signup", validateUserBody, createUser);
 
 router.use((request, response, next) => {
-  response.status(NOT_FOUND_STATUS).send({ message: INVALID_ROUTE_ERROR });
-  next();
+  next(new NotFoundError(INVALID_ROUTE_ERROR));
 });
 
 module.exports = router;
